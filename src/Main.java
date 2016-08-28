feb25c77190d4a469488afe93754b7323ba70202
Jacques Supcik <jacques.supcik@hefr.ch>
2016-08-28 21:39:35 +0000

    Rename variables

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,27 @@ public class Main {
     private final static String sticker = "facebook";
     private final static String message = "coffee kebab";
     private final static int NUMBER_OF_LETTERS = 'z' - 'a' + 1;
-    
-    
+      
     public static void main(String[] args) {
-        int[] stickerCount = new int[NUMBER_OF_LETTERS];
-        int[] messageCount = new int[NUMBER_OF_LETTERS];
+        int[] stickerLetters = new int[NUMBER_OF_LETTERS];
+        int[] messageLetters = new int[NUMBER_OF_LETTERS];
         
         for (int c: sticker.toLowerCase().toCharArray()) {
             if (c >= 'a' && c <= 'z')
-                stickerCount[c - 'a']++;
+                stickerLetters[c - 'a']++;
         }
         for (int c: message.toLowerCase().toCharArray()) {
             if (c >= 'a' && c <= 'z')
-                messageCount[c - 'a']++;
+                messageLetters[c - 'a']++;
         }
         int count = 0;
         for (int i = 0; i < NUMBER_OF_LETTERS; i++) {
-            if (messageCount[i] > 0) {
-                if (stickerCount[i] == 0) {
+            if (messageLetters[i] > 0) {
+                if (stickerLetters[i] == 0) {
                     System.out.println("Impossible!");
                     return;
                 }
-                int c = (int)((double)Math.ceil(messageCount[i] / (double)stickerCount[i]));
+                int c = (int)((double)Math.ceil(messageLetters[i] / (double)stickerLetters[i]));
                 if (c > count) {
                     count = c;
                 }
